c33a96cd7ad797516aa09a418da7a5b4bf55ffba
kzarek <k.zarek.906@studms.ug.edu.pl>
2022-05-27 10:17:02 +0200

    Fraction works

---
+++ src/tests/simpleTests.java
@@ -0,0 +1,23 @@
+package tests;
+
+import models.Fraction;
+import utils.FractionUtils;
+
+import java.math.BigInteger;
+
+public class simpleTests {
+    public static void main(String[] args) {
+        Fraction half = new Fraction(BigInteger.valueOf(-4), BigInteger.valueOf(8));
+        System.out.println(half);
+
+        Fraction different_half = new Fraction(BigInteger.valueOf(5), BigInteger.valueOf(10));
+        System.out.println(different_half);
+        System.out.println(FractionUtils.multiply(half, different_half));
+        Fraction one = new Fraction(BigInteger.valueOf(10), BigInteger.valueOf(10));
+        System.out.println(FractionUtils.add(different_half, one));
+        System.out.println(FractionUtils.minus(different_half, one));
+        System.out.println(FractionUtils.div(one, different_half));
+        System.out.println(FractionUtils.isGreater(different_half, half));
+    }
+
+}
