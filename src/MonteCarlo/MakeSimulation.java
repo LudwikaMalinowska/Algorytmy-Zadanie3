7cfb7367ccff805b4af9d2e4a3078125a24037fe
kzarek <k.zarek.906@studms.ug.edu.pl>
2022-06-12 17:42:04 +0200

    Working Monte Carlo Method

---
+++ src/MonteCarlo/MakeSimulation.java
@@ -0,0 +1,4 @@
+//package MonteCarlo;
+//
+//public class MakeSimulation {
+//}
--- src/MonteCarlo/MonteCarloMethod.java
+++ src/MonteCarlo/MonteCarloMethod.java
@@ -1,16 +1,24 @@
 package MonteCarlo;
 
+import javafx.util.Pair;
+
 import java.util.ArrayList;
+
+import static utils.GenerateMatrix.transitionFunction;
 
 public class MonteCarloMethod {
     private final Integer YesCount;
     private final Integer NoCount;
     private final Integer AllVotesCount;
+    private final Integer IterationsNumber;
+    private final ArrayList<String> simulationResult;
 
-    public MonteCarloMethod(Integer YesVotes,Integer NoVotes,Integer AllVotes){
+    public MonteCarloMethod(Integer YesVotes,Integer NoVotes,Integer AllVotes,Integer NumberOfIterations){
         this.YesCount=YesVotes;
         this.NoCount=NoVotes;
         this.AllVotesCount=AllVotes;
+        this.IterationsNumber =NumberOfIterations;
+        this.simulationResult= new ArrayList<>();
     }
 
     public ArrayList<Voter> generateVoters() {
@@ -27,10 +35,39 @@ public class MonteCarloMethod {
         return votersList;
     }
 
-    public boolean checkIfStateIsNotStable(ArrayList<Voter> voters){
+    public static boolean checkIfStateIsNotStable(ArrayList<Voter> voters){
         for (int i= 0; i< voters.size()-1;i++){
             if(!voters.get(i).getVote().equals(voters.get(i + 1).getVote())) return true;
         }
         return false;
+    }
+
+    public Double propability(ArrayList<String> votes){
+        double yesVotes=0d;
+        double allVotes=votes.size();
+        for ( String vote: votes){
+            if(vote.equals("Y")) yesVotes++;
+        }
+        return (yesVotes/allVotes);
+
+
+    }
+
+    public Double simulation(){
+        for(int i = 0; i< IterationsNumber; i++){
+            ArrayList<Voter> voters = generateVoters();
+            while (checkIfStateIsNotStable(voters)){
+                Voter first = voters.get((int) (Math.random() * voters.size()));
+                Voter second = voters.stream()
+                        .filter(e -> !e.equals(first))
+                        .toList()
+                        .get((int) (Math.random() * (voters.size() - 1)));
+                Pair<String, String> newPair = transitionFunction.get(new Pair<>(first.getVote(), second.getVote()));
+                first.setVote(newPair.getKey());
+                second.setVote(newPair.getValue());
+            }
+        this.simulationResult.add(voters.get(0).getVote());
+        }
+        return propability(simulationResult);
     }
 }
